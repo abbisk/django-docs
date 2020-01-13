021f99a2567396f5617204bac91a360c4bba9c18
abbisk <abc.com>
2020-01-13 15:33:05 +0530

    a shortcut: render() function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,15 @@
 from django.contrib import admin
 from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     # return HttpResponse("You are in poll's index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 # Create your views here.
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
