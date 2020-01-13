815578e8d07ef6a7510f0fc26a9af66598f620cc
abbisk <abc.com>
2020-01-13 15:17:19 +0530

    rendering templates to change the look of the page

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('data published')
+    pub_date = models.DateTimeField('date published')
+    objects = models.Manager()
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,20 @@
+from django.contrib import admin
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    # return HttpResponse("Hello, woeld. You are at the polls index.")
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+from .models import Question
 
+def index(request):
+    # return HttpResponse("You are in poll's index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+# Create your views here.
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
