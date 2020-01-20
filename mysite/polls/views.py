7816dc5097cc1da9b24b720943d472cde605a3ec
abbisk <abbiskpro@gmail.com>
2020-01-21 00:12:52 +0530

    amend views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404,render
 from .models import Question
 from .models import Choice, Question
+from django.views import generic
 from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def index(request):
@@ -11,16 +12,31 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 # added after app pert3
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
-    # return HttpResponse("You're voting on question %s." % question_id)
+    ... # same as above, no changes needed.
+
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
