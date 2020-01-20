ed477b28a79d482c841844e68888c0971d27ee61
abbisk <abbiskpro@gmail.com>
2020-01-20 16:39:30 +0530

    write a minimal form

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,22 +4,19 @@ from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 # Create your models here.
 class Question(models.Model):
-    try:
-        question_text = models.CharField(max_length=200)
-        pub_date = models.DateTimeField('date published')
-        objects = models.Manager()
-        def __str__(self):
-            return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    objects = models.Manager()
+    def __str__(self):
+        return self.question_text
 
     
-        def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    except ObjectDoesNotExist:
-        print(" ")
-        
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+           
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+        question = models.ForeignKey(Question,on_delete=models.CASCADE)
+        choice_text = models.CharField(max_length=200)
+        votes = models.IntegerField(default=0)
+        def __str__(self):
+            return self.choice_text
